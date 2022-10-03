d0b10dd80f0ce964b05e808587f66803e3162fb6
alexgaiser <alexcgaiser@gmail.com>
2022-10-02 23:26:50 -0400

    adds execution context for mlb service

--- server/src/main/scala/modules/providers/MLBClientModule.scala
+++ server/src/main/scala/modules/providers/MLBClientModule.scala
@@ -1,14 +1,17 @@
 package modules.providers
 
+
 import apis.MLBClient
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
+
+import scala.concurrent.ExecutionContext
 
 class MLBClientModule extends TwitterModule {
 
   @Singleton
   @Provides
-  def mlbClient()() = {
+  def mlbClient()(implicit executionContext: ExecutionContext) = {
     new MLBClient()
   }
 }
