9f3223f3e0e31a2d495dc2110e193b732f5f4850
alexgaiser <alexcgaiser@gmail.com>
2022-10-02 23:25:57 -0400

    adds implicit execution context

--- server/src/main/scala/providers/MLBService.scala
+++ server/src/main/scala/providers/MLBService.scala
@@ -1,15 +1,18 @@
 package providers
 
 import apis.MLBClient
+import com.twitter.util.Future
+import models.MLB.StandingsResponse
 
 import javax.inject.Inject
+import scala.concurrent.ExecutionContext
 
 
-case class MLBService @Inject()(mlbClient: MLBClient) {
+case class MLBService @Inject()(mlbClient: MLBClient)(implicit executionContext: ExecutionContext) {
   val name = "MLB Service"
 
-  def standings() = {
+  def standings(): Future[StandingsResponse] = {
     mlbClient.getStandings()
   }
-
 }
+
