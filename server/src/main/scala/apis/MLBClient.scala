4f73d9401f778643883de0666ea0ab31775ba436
alexgaiser <alexcgaiser@gmail.com>
2022-10-02 23:24:56 -0400

    update client to use sttp client

--- server/src/main/scala/apis/MLBClient.scala
+++ server/src/main/scala/apis/MLBClient.scala
@@ -1,13 +1,18 @@
 package apis
 
 import com.twitter.finagle.{Http, Service, http}
+import com.twitter.finatra.jackson.modules.ScalaObjectMapperModule
+import com.twitter.util.jackson.ScalaObjectMapper
+import models.MLB.StandingsResponse
+import utils.{RequestClient, SttpClient}
 
-class MLBClient(baseUrl: String = "statsapi.mlb.com:80/api/v1/standings/?leagueId=103") {
-  val client: Service[http.Request, http.Response] = Http.newService("statsapi.mlb.com:80")
+import java.net.URL
+import scala.concurrent.ExecutionContext
 
+class MLBClient(baseUrl: String = "statsapi.mlb.com:80/api/v1/standings/?leagueId=103")(implicit executionContext: ExecutionContext) {
   def getStandings() = {
-    val request = http.Request(http.Method.Get, "/api/v1/standings/?leagueId=103")
-    request.host("statsapi.mlb.com:80")
-    client(request)
+    val url = new URL("http://statsapi.mlb.com:80/api/v1/standings/?leagueId=103")
+    SttpClient().get[StandingsResponse](url)
   }
 }
+
