5c04b544feffcd6d440984c906a9bf618dcc36c8
alexgaiser <alexcgaiser@gmail.com>
2022-10-09 19:08:36 -0400

    update standings model

--- server/src/main/scala/models/MLB/StandingsModel.scala
+++ server/src/main/scala/models/MLB/StandingsModel.scala
@@ -1,23 +1,40 @@
 package models.MLB
 
-case class StandingsResponse(copyright: String)
+case class StandingsResponse(
+                              copyright: String,
+                              records: List[MLBRecord]
+                            )
 
 case class TeamRecord(
                        team: MLBTeam,
                        season: Int,
-                       clinchIndicator: String,
+                       clinchIndicator: Option[String],
                        divisionRank: Int,
-                       gamesPlayed: Int
+                       gamesPlayed: Int,
+                       records: GameRecords
+
                      )
 
-case class Record(
-                   standingsType: String,
-                   league: MLBLeague,
-                   division: MLBDivision,
-                   sport: MLBSport,
-                   teamRecords: List[TeamRecord],
-                   leagueRecord: LeagueRecord
-                 )
+case class MLBRecord(
+                      standingsType: String,
+                      league: MLBLeague,
+                      division: MLBDivision,
+                      sport: MLBSport,
+                      teamRecords: List[TeamRecord],
+                    )
+
+
+case class GameRecords(
+                        splitRecords: List[SplitRecord]
+                      )
+
+case class SplitRecord(
+                        wins: Int,
+                        losses: Int,
+                        `type`: String,
+                        pct: Double
+                      )
+
 
 trait MLBScope {
   val id: Int
@@ -30,4 +47,4 @@ case class MLBDivision(id: Int, link: String) extends MLBScope
 
 case class MLBSport(id: Int, link: String) extends MLBScope
 
-case class LeagueRecord(wins: Int, losses: Int, ties: Int, pct: Float)
+case class LeagueRecord(wins: Int, losses: Int, ties: Int, pct: Double)
