17696e39e82375555f07891cec35a02b1f98f63a
alexgaiser <alexcgaiser@gmail.com>
2023-04-23 13:51:28 -0400

    reformat StandingsController

--- server/src/main/scala/controllers/StandingsController.scala
+++ server/src/main/scala/controllers/StandingsController.scala
@@ -7,16 +7,14 @@ import providers.MLBService
 
 import javax.inject.Inject
 
-case class StandingsController @Inject()(mlbProvider: MLBService) extends Controller {
-  get("/ping") { request: Request =>
-    "pong"
-  }
+case class StandingsController @Inject() (mlbProvider: MLBService) extends Controller {
+  get("/ping") { request: Request => "pong" }
 
   get("/name") { request: Request =>
     println("hello")
     response.ok.body("Bob")
   }
-  
+
   get("/standings") { request: Request =>
     val standings = Await.result(mlbProvider.standings())
     standings
